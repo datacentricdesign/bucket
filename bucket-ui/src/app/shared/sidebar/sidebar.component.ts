2ab78d6aaad5ee68cc4314b0613bd669ccc0cf87
Jacky Bourgeois <J.Bourgeois@tudelft.nl>
2020-08-10 14:49:34 +0200

    setting prod api url

--- bucket-ui/src/app/shared/sidebar/sidebar.component.ts
+++ bucket-ui/src/app/shared/sidebar/sidebar.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { Thing } from '@datacentricdesign/types';
 import { catchError, map } from 'rxjs/operators';
+import { AppService } from 'app/app.service';
 
 export interface RouteInfo {
     path: string;
@@ -24,16 +25,20 @@ export const ROUTES: RouteInfo[] = [dashboardRoute];
 export class SidebarComponent implements OnInit {
     public menuItems: any[];
 
+    private apiURL: string
+
     things$: Observable<Thing[]>;
     things: Thing[]
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private appService: AppService) {
+        this.apiURL = appService.settings.apiURL;
+    }
 
 
     ngOnInit() {
         this.menuItems = ROUTES.filter(menuItem => menuItem);
 
-        this.http.get("https://dwd.tudelft.nl/bucket/api/things").subscribe((data: any) => {
+        this.http.get(this.apiURL + "/things").subscribe((data: any) => {
             console.log(data)
             for (let index = 0; index < data.length; index++) {
                 this.menuItems.push({ path: 'thing/' + data[index].id, title: data[index].name, icon: 'nc-bank', class: '' })
--- bucket-ui/src/app/shared/thing-form/thing-form.component.ts
+++ bucket-ui/src/app/shared/thing-form/thing-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DTOThing, DTORaspberryPi } from '@datacentricdesign/types';
 import { HttpClient } from '@angular/common/http';
+import { AppService } from 'app/app.service';
 
 @Component({
   selector: 'app-thing-form',
@@ -12,6 +13,8 @@ export class ThingFormComponent implements OnInit {
   types = ['GENERIC', 'RASPBERRYPI'];
 
   submitted = false;
+
+  private apiURL: string
 
   model:DTOThing = {
     name: 'My Test Thing',
@@ -29,15 +32,14 @@ export class ThingFormComponent implements OnInit {
   }
 
   onSubmit() { 
-    this.http.post("https://dwd.tudelft.nl/bucket/api/things", this.model).subscribe((data: any) => {
+    this.http.post(this.apiURL + "/things", this.model).subscribe((data: any) => {
       console.log(data)
     });
   }
 
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private appService: AppService) {
+    this.apiURL = this.appService.settings.apiURL;
+  }
 
   ngOnInit(): void {
   }
