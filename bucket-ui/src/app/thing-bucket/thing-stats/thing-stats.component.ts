358150486e8beec1006a1313aa4733f00330ea37
Jacky Bourgeois <J.Bourgeois@tudelft.nl>
2020-09-19 00:49:37 +0200

    generate color that fit the theme for charts

--- bucket-ui/src/app/thing-bucket/thing-stats/thing-stats.component.ts
+++ bucket-ui/src/app/thing-bucket/thing-stats/thing-stats.component.ts
@@ -11,6 +11,57 @@ import * as moment from 'moment'
 })
 export class ThingStatsComponent implements OnInit {
 
+  colors = [
+    "rgb(81,203,206)",
+    "rgb(107,208,152)",
+    "rgb(81,188,218)",
+    "rgb(251,198,88)",
+    "rgb(239,129,87)",
+    "rgb(102,102,102)",
+    "rgb(193,120,193)",
+    "rgb(41,102,103)",
+    "rgb(54,104,76)",
+    "rgb(41,94,109)",
+    "rgb(126,99,44)",
+    "rgb(120,65,44)",
+    "rgb(51,51,51)",
+    "rgb(97,60,97)",
+    "rgb(168,229,231)",
+    "rgb(181,232,204)",
+    "rgb(168,222,237)",
+    "rgb(253,227,172)",
+    "rgb(247,192,171)",
+    "rgb(179,179,179)",
+    "rgb(224,188,224)",
+    "rgb(20,51,52)",
+    "rgb(27,52,38)",
+    "rgb(20,47,55)",
+    "rgb(63,50,22)",
+    "rgb(60,32,22)",
+    "rgb(26,26,26)",
+    "rgb(48,30,48)",
+    "rgb(212,242,243)",
+    "rgb(218,243,229)",
+    "rgb(212,238,246)",
+    "rgb(254,241,213)",
+    "rgb(251,224,213)",
+    "rgb(217,217,217)",
+    "rgb(240,221,240)",
+    "rgb(61,152,155)",
+    "rgb(80,156,114)",
+    "rgb(61,141,164)",
+    "rgb(188,149,66)",
+    "rgb(179,97,65)",
+    "rgb(77,77,77)",
+    "rgb(145,90,145)",
+    "rgb(125,216,218)",
+    "rgb(144,220,178)",
+    "rgb(125,205,227)",
+    "rgb(252,212,130)",
+    "rgb(243,161,129)",
+    "rgb(140,140,140)",
+    "rgb(209,154,209)",
+  ]
 
   public canvas: any;
   public ctx;
@@ -38,11 +89,12 @@ export class ThingStatsComponent implements OnInit {
     let legend = ''
     let colors = []
     let labels = []
+    let colorIndex = 0;
     for (let i = 0; i < things.length; i++) {
       for (let j = 0; j < things[i].properties.length; j++) {
         const values = things[i].properties[j].values
         if (types[things[i].properties[j].type.id] === undefined) {
-          const color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
+          const color = this.colors[colorIndex]
           colors.push(color)
           labels.push(things[i].properties[j].type.name)
           legend += '<i class="fa fa-circle" style="color:' + color + ';padding-left:3px"></i> ' + things[i].properties[j].type.name + '<br>'
@@ -55,6 +107,7 @@ export class ThingStatsComponent implements OnInit {
           }
           types[things[i].properties[j].type.id] += sum
         }
+        colorIndex++
       }
     }
 
@@ -133,6 +186,7 @@ export class ThingStatsComponent implements OnInit {
 
     let legend = ''
 
+    let colorIndex = 0
     for (let i = 0; i < things.length; i++) {
       for (let j = 0; j < things[i].properties.length; j++) {
         const values = things[i].properties[j].values
@@ -147,7 +201,7 @@ export class ThingStatsComponent implements OnInit {
           points.push(sum)
         }
         if (labels.length < labelTime.length) labels = labelTime;
-        const color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        const color = this.colors[colorIndex];
         legend += '<i class="fa fa-circle" style="color:' + color + ';padding-left:3px"></i> ' + things[i].properties[j].name
         datasets.push({
           label: things[i].properties[j].name,
@@ -160,6 +214,7 @@ export class ThingStatsComponent implements OnInit {
           pointHoverRadius: 3,
           pointBorderWidth: 8
         })
+        colorIndex++
       }
     }
 
