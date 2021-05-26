f4fb2f046a8241cea951c53084e3e48fac2bf0c8
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2021-05-26 07:01:26 +0200

    test safe log

--- bucket-api/src/Logger.ts
+++ bucket-api/src/Logger.ts
@@ -35,13 +35,27 @@ export class Log {
       "debug"
     );
 
-    Log.silly = Log.logger.silly.bind(Log.logger)
-    Log.debug = Log.logger.debug.bind(Log.logger)
-    Log.trace = Log.logger.trace.bind(Log.logger)
-    Log.info = Log.logger.info.bind(Log.logger)
-    Log.warn = Log.logger.warn.bind(Log.logger)
-    Log.error = Log.logger.error.bind(Log.logger)
-    Log.fatal = Log.logger.fatal.bind(Log.logger)
+    Log.silly = (...args: unknown[]): ILogObject => {
+      return Log.logger.silly(Log.convertArgObjectToString(args))
+    }
+    Log.debug = (...args: unknown[]): ILogObject => {
+      return Log.logger.debug(Log.convertArgObjectToString(args))
+    }
+    Log.trace = (...args: unknown[]): ILogObject => {
+      return Log.logger.trace(Log.convertArgObjectToString(args))
+    }
+    Log.info = (...args: unknown[]): ILogObject => {
+      return Log.logger.info(Log.convertArgObjectToString(args))
+    }
+    Log.warn = (...args: unknown[]): ILogObject => {
+      return Log.logger.warn(Log.convertArgObjectToString(args))
+    }
+    Log.error = (...args: unknown[]): ILogObject => {
+      return Log.logger.error(Log.convertArgObjectToString(args))
+    }
+    Log.fatal = (...args: unknown[]): ILogObject => {
+      return Log.logger.fatal(Log.convertArgObjectToString(args))
+    }
 
     // Make sure there is a subfolder to store images and logs
     try {
@@ -50,36 +64,47 @@ export class Log {
       if (error && error.errno !== -17) {
         try {
           return Log.error(error)
-        }  catch (error) {
+        } catch (error) {
           console.error(error)
         }
       }
     }
-
   }
+
+  static convertArgObjectToString(args: unknown[]): unknown[] {
+    const argsStr = []
+    for (const arg of args) {
+      if (typeof arg === "object" && arg !== null) {
+        argsStr.push(Log.safeStringify(arg))
+      } else {
+        argsStr.push(arg)
+      }
+    }
+    return argsStr;
+  }
+  // safely handles circular references
+  static safeStringify(obj, indent = 2) {
+    let cache = [];
+    const retVal = JSON.stringify(
+      obj,
+      (key, value) =>
+        typeof value === "object" && value !== null
+          ? cache.includes(value)
+            ? "[Duplicate reference removed to avoid circularity]" // Duplicate reference found, discard key
+            : cache.push(value) && value // Store value in our collection
+          : value,
+      indent
+    );
+    cache = null;
+    return retVal;
+  };
 }
 
-// safely handles circular references
-function safeStringify(obj, indent = 2) {
-  let cache = [];
-  const retVal = JSON.stringify(
-    obj,
-    (key, value) =>
-      typeof value === "object" && value !== null
-        ? cache.includes(value)
-          ? "[Duplicate reference removed to avoid circularity]" // Duplicate reference found, discard key
-          : cache.push(value) && value // Store value in our collection
-        : value,
-    indent
-  );
-  cache = null;
-  return retVal;
-};
 
 function logToTransport(logObject: ILogObject) {
   try {
-    appendFileSync(config.hostDataFolder + '/logs/' + moment(new Date()).format('YYYY-MM-DD_HH') + '.log', safeStringify(logObject) + "\n");
-  } catch(error) {
+    appendFileSync(config.hostDataFolder + '/logs/' + moment(new Date()).format('YYYY-MM-DD_HH') + '.log', Log.safeStringify(logObject) + "\n");
+  } catch (error) {
     console.log(error)
   }
 }
