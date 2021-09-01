bcb3a7f6dbd3cde2a55acca412e76992d77ad63c
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2021-09-01 17:59:02 +0200

    debug count url

--- bucket-api/src/policy/PolicyService.ts
+++ bucket-api/src/policy/PolicyService.ts
@@ -228,7 +228,7 @@ export class PolicyService {
     try {
       while (lastResultSize == 500) {
         const res = await fetch(url + fullList.length, options);
-        console.log(url)
+        console.log(url + fullList.length)
         if (res.ok) {
           let result = await res.json();
           if (result === null) {
